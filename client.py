49e604ede476bfcc9d0051227ab470242a372144
Greentea1337 <110815737+Greentea1337@users.noreply.github.com>
2023-03-27 14:14:01 +0300

    Добавлен выбор ай-пи адреса и порта во время ввода ника

--- client.py
+++ client.py
@@ -14,14 +14,30 @@ class NicknameDialog(QDialog):
         self.setWindowTitle('Enter Nickname')
         self.setModal(True)
 
+        # Create IP address input textbox and label
+        self.ip_input = QLineEdit(self)
+        self.ip_label = QLabel('IP Address:', self)
+        self.ip_input.setText('127.0.0.1')
+
+        # Create port input textbox and label
+        self.port_input = QLineEdit(self)
+        self.port_label = QLabel('Port:', self)
+        self.port_input.setText('8080')
+
         # Create nickname input textbox and OK button
+        self.nickname_label = QLabel("Nickname:")
         self.nickname_input = QLineEdit(self)
         self.ok_button = QPushButton('OK', self)
 
-        # Create layout for nickname input and OK button
-        input_layout = QHBoxLayout()
-        input_layout.addWidget(self.nickname_input)
-        input_layout.addWidget(self.ok_button)
+        # Create layout for IP address, port, nickname input, and OK button
+        input_layout = QGridLayout()
+        input_layout.addWidget(self.ip_label, 0, 0)
+        input_layout.addWidget(self.ip_input, 0, 1)
+        input_layout.addWidget(self.port_label, 1, 0)
+        input_layout.addWidget(self.port_input, 1, 1)
+        input_layout.addWidget(self.nickname_label, 2, 0)
+        input_layout.addWidget(self.nickname_input, 2, 1)
+        input_layout.addWidget(self.ok_button, 3, 1)
 
         # Set dialog layout
         self.setLayout(input_layout)
@@ -29,9 +45,15 @@ class NicknameDialog(QDialog):
         # Connect OK button to accept() method
         self.ok_button.clicked.connect(self.accept)
 
+    def get_ip_address(self):
+        return self.ip_input.text()
+
+    def get_port(self):
+        return self.port_input.text()
+
     def get_nickname(self):
         return self.nickname_input.text()
-
+        
 class ClientGUI(QWidget):
     def __init__(self):
         super().__init__()
@@ -67,6 +89,20 @@ class ClientGUI(QWidget):
         # Set main layout
         self.setLayout(main_layout)
 
+        # Create socket object and connect to server
+        nickname_dialog = NicknameDialog()
+        if nickname_dialog.exec_():
+            ip_address = nickname_dialog.get_ip_address()
+            port = nickname_dialog.get_port()
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((ip_address, int(port)))
+            self.nickname = nickname_dialog.get_nickname()
+            self.client_socket.send(self.nickname.encode('utf-8'))
+
+            # Create thread to receive messages from server
+            receive_thread = threading.Thread(target=self.receive_message)
+            receive_thread.start()
+
         # Connect send button to send_message() method
         self.send_button.clicked.connect(self.send_message)
 
@@ -75,20 +111,6 @@ class ClientGUI(QWidget):
 
         # Connect exit button to exit_chat() method
         self.exit_button.clicked.connect(self.exit_chat)
-
-        # Create socket object and connect to server
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(('127.0.0.1', 8080))
-
-        # Prompt user to enter nickname and send to server
-        nickname_dialog = NicknameDialog()
-        nickname_dialog.exec_()
-        self.nickname = nickname_dialog.get_nickname()
-        self.client_socket.send(self.nickname.encode('utf-8'))
-
-        # Create thread to receive messages from server
-        receive_thread = threading.Thread(target=self.receive_message)
-        receive_thread.start()
 
     def receive_message(self):
         while True:
